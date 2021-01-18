ee53fa908af1e8529ab9904c23d4b84009f33296
USERNAME <MY_NAME@example.com>
2021-01-19 00:00:58 +0100

    AZIZ JUSTE KHDEMT UN PETIT PROGRAMME JS NCHALLAH YAJBEK ENTY W LOULED

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What year was the first iPhone released ?",
+    answer: "2005",
+    options: [
+      "2000",
+      "2005",
+      "2007",
+      "2010"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the biggest difference about the iPhone X ?",
+    answer: "It has no home button",
+    options: [
+      "It glows in the dark",
+      "It Has 5 cameras",
+      "It can stand up by itself",
+      "It has no home button"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What year was the first iPad released ?",
+    answer: "2010",
+    options: [
+      "2008",
+      "2012",
+      "2010",
+      "2014"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of these Steve Jobs quotes on business is real ?",
+    answer: "Great things in business are never done by one person.",
+    options: [
+      "Great things in business are never done by one person.",
+      "Leading this business was never a question for me.",
+      "Business only thrives when employees are thriving in their personal lives.",
+      "Inquiring about business dealings is necessary to succeed."
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "Which Apple product is the most popular ?",
+    answer: "iPhone",
+    options: [
+      "Macbook",
+      "iPhone",
+      "iPad",
+      "iPod"
+    ]
+  },
+];
